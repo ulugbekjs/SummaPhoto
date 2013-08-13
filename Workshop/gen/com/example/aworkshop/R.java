c72002d68662a27b435ebcaffd385d53d299c744
ywilkof <ywilkof@gmail.com>
2013-08-13 12:19:02 +0300

    removed MainActivity

--- Workshop/gen/com/example/aworkshop/R.java
+++ Workshop/gen/com/example/aworkshop/R.java
@@ -24,16 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int radioDaily=0x7f080003;
         public static final int radioMode=0x7f080001;
-        public static final int radioOff=0x7f080004;
+        public static final int radioOff=0x7f080005;
         public static final int radioSmart=0x7f080002;
+        public static final int timePicker1=0x7f080004;
         public static final int tv=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_settings=0x7f030001;
+        public static final int activity_settings=0x7f030000;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- Workshop/src/com/example/aworkshop/SettingsActivity.java
+++ Workshop/src/com/example/aworkshop/SettingsActivity.java
@@ -1,17 +1,55 @@
 package com.example.aworkshop;
 
+import java.io.File;
+
 import org.joda.time.DateTime;
 
+import ActivationManager.SmartModeService;
+import PhotoListener.PhotoListenerThread;
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.view.Menu;
 
 public class SettingsActivity extends Activity {
+	
+	// static final fields
+	public static final File ROOT = new File(Environment.getExternalStorageDirectory(), "DCIM");
+	//		File dataDirectory = new File(root + "/DCIM/Camera/");
+	private static final String  PHOTO_DIR = ROOT + File.separator + "Camera" + File.separator;
 
+	// global fields
+	PhotoListenerThread observer;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+	
+		observer = new PhotoListenerThread(PHOTO_DIR); // observer over the gallery directory
+		
+		observer.startWatching();
+		SmartModeService.startService();
+//		File directory = new File("E:/Pictures");
+//		if (!directory.exists())
+//			return;
+//		File[] arrayOfPic =  directory.listFiles();
+//		Photo tempPhoho = null;
+//		List<Photo> photosToCluster = new LinkedList<Photo>(); 
+//		for (File file : arrayOfPic)
+//		{
+//			try
+//			{
+//				tempPhoho = PhotoListenerThread.createPhotoFromFile(file.getAbsolutePath());
+//			}
+//			catch (Exception ex)
+//			{
+//			}
+//			if (tempPhoho != null)
+//				photosToCluster.add(tempPhoho);
+//		}
+//
+		
 	}
 
 	@Override
