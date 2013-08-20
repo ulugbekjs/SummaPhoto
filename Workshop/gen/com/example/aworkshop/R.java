f143fa7737721cdb6a82e5234e439399ea7766ab
ywilkof <ywilkof@gmail.com>
2013-08-20 16:54:34 +0300

    icin

--- Workshop/gen/com/example/aworkshop/R.java
+++ Workshop/gen/com/example/aworkshop/R.java
@@ -22,6 +22,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
@@ -39,18 +40,17 @@ public final class R {
         public static final int settings=0x7f070001;
     }
     public static final class string {
-        public static final int Daily=0x7f050005;
-        public static final int DailyDescription=0x7f050008;
-        public static final int Mode=0x7f05000a;
-        public static final int ModeHeader=0x7f05000b;
-        public static final int Off=0x7f050006;
-        public static final int OffDescription=0x7f050009;
-        public static final int Smart=0x7f050004;
-        public static final int SmartDescription=0x7f050007;
+        public static final int Daily=0x7f050004;
+        public static final int DailyDescription=0x7f050007;
+        public static final int Mode=0x7f050009;
+        public static final int ModeHeader=0x7f05000a;
+        public static final int Off=0x7f050005;
+        public static final int OffDescription=0x7f050008;
+        public static final int Smart=0x7f050003;
+        public static final int SmartDescription=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_settings=0x7f050003;
+        public static final int title_activity_settings=0x7f050002;
     }
     public static final class style {
         /** 
