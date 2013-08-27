3049f61b9b0840f5c75667f5c17781f087672c0a
ywilkof <ywilkof@gmail.com>
2013-08-28 02:18:06 +0300

    splash screen

---
+++ Workshop/src/com/example/aworkshop/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.aworkshop;
+
+import org.joda.time.DateTime;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ * 
+ * @see SystemUiHider
+ */
+public class SplashActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_splash);
+		
+		// load jodatime to memory - takes 2 seconds
+		DateTime.now();
+		
+		closeScreen();
+	}
+	
+	private void closeScreen() {
+        Intent lIntent = new Intent();
+        lIntent.setClass(this, SettingsActivity.class);
+        startActivity(lIntent);
+        finish();
+    }
+
+	
+}
