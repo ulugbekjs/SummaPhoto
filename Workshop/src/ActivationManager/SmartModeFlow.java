a99bcf5101fe5e8a952b6ab93997828d447885e8
ywilkof <ywilkof@gmail.com>
2013-09-04 10:54:32 +0300

    merge

---
+++ Workshop/src/ActivationManager/SmartModeFlow.java
@@ -0,0 +1,177 @@
+package ActivationManager;
+
+import java.io.FileNotFoundException;
+import java.security.PublicKey;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.example.aworkshop.SettingsActivity;
+
+import android.app.Service;
+import android.util.Log;
+import Common.ActualEventsBundle;
+import Common.Photo;
+import Common.PhotoContainer;
+import Common.Utils;
+import Generator.AbstractBuilder;
+import Generator.AbstractTemplate;
+import Generator.BlockCollageBuilder;
+import Generator.MapCollageBuilder;
+import Partitioning.DBScan;
+
+public class SmartModeFlow {
+
+	private static final String TAG = SmartModeFlow.class.getName();
+//	private static ScheduledExecutorService scheduler = null;
+	private static ExecutorService scheduler = null;
+	private static boolean busy = false;
+	private static ActivationManager manager = ActivationManager.getInstance();  
+//	private static final int INTERVAL_IN_SECONDS = 30;
+
+	private SmartModeFlow() {
+	}
+
+	public static void startFlow() {
+//		if (scheduler == null) {
+//			scheduler =  Executors.newScheduledThreadPool(1);
+//			// waits INTERVAL_IN_SECONDS seconds after end of last execution
+//			scheduler.scheduleWithFixedDelay(new Runnable() {
+//				@Override
+//				public void run() { // this is the main flow of the app
+//
+//					manager.consumeDedictedRequests(); 
+//					boolean collageNeeded = manager.processPhotoBuffer();
+//
+//					boolean successful = true;
+//					if (collageNeeded) { // ActivationManager decided clustering should be made
+//						ActualEventsBundle events = partiotionToEvents();
+//
+//						// build the collage from Bundle of photos
+//						Photo collage = null;
+//						if (SettingsActivity.COLLAGE_TYPE == AbstractTemplate.BLOCK_TYPE) {
+//							successful &= buildCollage(collage, new BlockCollageBuilder(events));
+//						}
+//						if (SettingsActivity.COLLAGE_TYPE == AbstractTemplate.MAP_TYPE) {
+//							successful &= buildCollage(collage, new MapCollageBuilder(events));
+//						}
+//						if (successful) {
+//							try {
+//								Utils.notifyUserCollageCreated(collage);
+//							} catch (FileNotFoundException e) {
+//								Log.e(TAG, "Could not open the created collage file, collage notification aborted.");
+//							}
+//						}
+//						else { // there was an error, notify user
+//							Utils.notifyUserWithError("Error when building collage",
+//									"Summaphoto has failed building your collage.");
+//						}
+//
+//					}
+//					else {
+//						// do nothing, advance to next iteration
+//					}
+//				}
+//
+//				/**
+//				 * run the DBScan algorithm to cluster photos to ActualEvents
+//				 * @return
+//				 */
+//				private ActualEventsBundle partiotionToEvents() {
+//					DBScan eventsClusterer = new DBScan(PhotoContainer.getInstance().getProcessedPhotos());
+//					ActualEventsBundle events = eventsClusterer.runDBScanAlgorithm();
+//					return events;
+//				}
+//			},
+//			20,
+//			INTERVAL_IN_SECONDS,
+//			TimeUnit.SECONDS);	
+		if (!busy) {
+			scheduler = Executors.newSingleThreadExecutor();
+			scheduler.execute(new Runnable() {
+				
+				@Override
+				public void run() {
+
+					setBusy(true);					
+					try {
+						Thread.sleep(10000);
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					manager.consumeDedictedRequests(); 
+					boolean collageNeeded = manager.processPhotoBuffer();
+
+					if (collageNeeded) { // ActivationManager decided clustering should be made
+						ActualEventsBundle events = partitionToEvents();
+
+						// build the collage from Bundle of photos
+						ResultPair result = null;
+						
+						if (SettingsActivity.COLLAGE_TYPE == AbstractTemplate.BLOCK_TYPE) {
+							result =  buildCollage(new BlockCollageBuilder(events));
+						}
+						if (SettingsActivity.COLLAGE_TYPE == AbstractTemplate.MAP_TYPE) {
+							result = buildCollage(new MapCollageBuilder(events));
+						}
+						if (result.validCollage) {
+							try {
+								Utils.notifyUserCollageCreated(result.collage);
+							} catch (FileNotFoundException e) {
+								Log.e(TAG, "Could not open the created collage file, collage notification aborted.");
+							}
+						}
+
+					}
+					
+					setBusy(false);
+				}
+
+				/**
+				 * run the DBScan algorithm to cluster photos to ActualEvents
+				 * @return
+				 */
+				private ActualEventsBundle partitionToEvents() {
+					DBScan eventsClusterer = new DBScan(PhotoContainer.getInstance().getProcessedPhotos());
+					ActualEventsBundle events = eventsClusterer.ComputeCluster();
+					return events;
+				}
+			});
+		}
+	}
+
+	public synchronized static void stopService() {
+//		scheduler.shutdown();
+		scheduler = null;
+		busy = false;
+	}
+	
+	private synchronized static void setBusy(boolean busy) {
+		SmartModeFlow.busy = busy;
+	}
+	
+	public static synchronized boolean isFlowRunning() {
+//		return (scheduler != null);
+		return busy;
+	}
+
+	private static ResultPair buildCollage(AbstractBuilder builder) {
+		boolean successful;
+		Photo collage = null;
+		DedicatedRequest request = builder.setTemplate();
+		if (request != null) { // not enough photos for collage
+			manager.addRequestToBuffer(request);
+			successful = false;
+		}
+		else { 
+			builder.populateTemplate();
+			collage =  builder.buildCollage();
+			successful = true;
+		}
+		return new ResultPair(successful, collage);
+	}
+	
+}
---
+++ Workshop/src/Generator/Line.java
@@ -0,0 +1,149 @@
+package Generator;
+
+/**
+ * This class represents a line; SOULD NOT BE USED for line with undefined slope (line that is vertical to X-axis)
+ * @author omri
+ *
+ */
+public class Line {
+	
+	private double slope;
+	
+	private double constant;
+	
+	private PixelPoint fromPoint;
+	private PixelPoint toPoint;
+	
+	private boolean isUndefinedSlope = false;
+	
+	public Line (PixelPoint fromPoint, PixelPoint toPoint)
+	{
+		this.fromPoint = fromPoint;
+		this.toPoint = toPoint;
+		slope = calculateSlope();
+		constant = calculateConst();
+	}
+	
+	public double getSlope() {
+		return slope;
+	}
+	
+	public double getConstant() {
+		return constant;
+	}
+	
+	public int getLineXDelta() {
+		return Math.abs(toPoint.getX() - fromPoint.getX());
+	}
+	
+	public int getLineYDelta() {
+		return Math.abs(toPoint.getY() - fromPoint.getY());
+	}
+	
+	
+	/**
+	 * @param x
+	 * @return the value of y of the line
+	 */
+	
+	public double getY (double x)
+	{
+		if (getSlope() == Double.NaN)
+			return Double.NaN;
+		return (x * getSlope() + constant);
+	}
+	
+	
+	public PixelPoint getFromPoint() {
+		return fromPoint;
+	}
+	
+	public PixelPoint getToPoint() {
+		return toPoint;
+	}
+	
+	/** 
+	 * @return the angle between Y-axis to the line from pointA to pointB. The value is [0...360]
+	 * while 0 is the north, 90 is the east etc.
+	 */
+	
+	public double getTetaFromYAxis ()
+	{
+				
+		// in case that the line is vertical to the x-Axis the the slope is undefined but the angle is 0
+		if (isUndefinedSlope)
+		{
+			return 0;
+		}
+		float angle = (float) Math.toDegrees(Math.atan2(toPoint.getX() - fromPoint.getX(), toPoint.getY() - fromPoint.getY()));
+
+	    if(angle < 0){
+	        angle += 360;
+	    }
+
+	    return angle;
+	}
+
+	/**
+	 * @return calculated slop of the line. If topLeft/buttomRight is null, or if the line is vertical to the X-axis
+	 * NaN is returned
+	 */
+	private Double calculateSlope ()
+	{
+		Double deltaY;
+		Double deltaX;
+		Double slope;
+		if ((fromPoint == null) || (toPoint == null))
+		{
+			return Double.NaN;
+		}
+		if (fromPoint.getX() ==  toPoint.getX())
+		{
+			isUndefinedSlope = true;
+			return Double.NaN;
+		}
+			
+		if (fromPoint.getX() > toPoint.getX() ){
+			deltaY = (double) (fromPoint.getY() - toPoint.getY());
+			deltaX = (double) (fromPoint.getX() - toPoint.getX());
+		}
+		else {
+			deltaY = (double) (toPoint.getY() - fromPoint.getY() );
+			deltaX = (double) (toPoint.getX()- fromPoint.getX());
+		}
+		slope = deltaY / deltaX;
+		return slope;
+	}
+	/**
+	 * @return the constant of the line. If the slope is undefined retrun NaN
+	 */
+	private Double calculateConst()
+
+	{
+		if (isUndefinedSlope)
+			return Double.NaN;
+		return (fromPoint.getY() - constant*fromPoint.getX());
+	}
+	
+	
+	/**
+	 * @param point
+	 * @return whether the point is above this line. If the scope is undefined, returns true if the x coordinate of the point is
+	 * bigger than the x coordinate of the line
+	 */
+	public Boolean isPointAboveLine (PixelPoint point)
+	{
+		if (point == null)
+			return false;
+		if (isUndefinedSlope)
+		{
+			return  (point.getX()> fromPoint.getX()); 
+		}
+		if (point.getX() * slope + constant < point.getY())
+			return true;
+		return false;
+	}
+
+	
+
+}
